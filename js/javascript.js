080a9b978e615052600ca23f63c18cb7846cbae7
dima <dimadiplom2020@bk.ru>
2021-07-01 15:31:34 +0300

    11

--- js/javascript.js
+++ js/javascript.js
@@ -9,20 +9,26 @@
 
 
 
-document.getElementById('btnsearch').onclick = function() {
-    document.getElementById('searchhid').style.display= 'none';
+document.getElementById('menu__alt03btn').onclick = function() {
+    document.getElementById('header').style.display= 'none';
     document.getElementById('menuhid').style.display= 'block';
   }
 
   document.getElementById('btnmenu').onclick = function() {
-    document.getElementById('menuhid').style.display= 'hidden';
-    document.getElementById('searchhid').style.display= 'block';
+    document.getElementById('menuhid').style.display= 'none';
+    document.getElementById('header').style.display= 'block';
   }
 
 
+  document.getElementById('searchbtnn').onclick = function() {
+    document.getElementById('header').style.display= 'none';
+    document.getElementById('searchhid').style.display= 'block';
+  }
 
-
-
+  document.getElementById('btnsearch').onclick = function() {
+    document.getElementById('searchhid').style.display= 'none';
+    document.getElementById('header').style.display= 'block';
+  }
 
 
 
