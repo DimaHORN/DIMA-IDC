626787d6e55dcc4392d346202eeff8229012613d
dima <dimadiplom2020@bk.ru>
2021-07-01 13:04:13 +0300

    8

---
+++ js/javascript.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('btnsearch').onclick = function() {
+    document.getElementById('searchhid').style.display= 'none';
+    document.getElementById('menuhid').style.display= 'block';
+  }
+
+  document.getElementById('btnmenu').onclick = function() {
+    document.getElementById('menuhid').style.display= 'hidden';
+    document.getElementById('searchhid').style.display= 'block';
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
